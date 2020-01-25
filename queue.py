c7a33b8130943ac58db3bce71ede29ac227ab6d7
shivi123gangwar <32398611+shivi123gangwar@users.noreply.github.com>
2020-01-25 19:00:26 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.stk = []
+    def push(self, value):
+        self.stk.append(value)
+    def pop(self):
+        return self.stk.pop(0)
+    def disply(self):
+        return self.stk
+stkOperations = Stack()
+stkOperations.push(10)
+stkOperations.push(20)
+stkOperations.push(30)
+stkOperations.push(40)
+print(stkOperations.disply())
+print()
+stkOperations.pop()
+print(stkOperations.disply())
+
